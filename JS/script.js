df344800538042f13a3f50b84052387b41ce9c17
Euan <euanconlon46@gmail.com>
2023-10-12 15:29:33 +0100

    Start on adding dynamically loaded header

--- JS/script.js
+++ JS/script.js
@@ -34,3 +34,16 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     xhr.send();
 });
+
+// dynamically loads the header using xhr
+window.addEventListener('DOMContentLoaded', function() {
+    var headerPlaceholder = document.getElementById("headerPlaceholder");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "header.html", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            headerPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+});
