ab5100d512129849be1cfff107b9b7faa240280e
Euan <euanconlon46@gmail.com>
2023-10-12 15:20:27 +0100

    Footer now Dynamically loaded
    Done using an xhr in my script.js file

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,15 @@ function changeSwatchImage(imageSrc) {
     var image = document.getElementById("swatchImage");
     image.src = imageSrc;
 }
+
+window.addEventListener('DOMContentLoaded', function() {
+    var footerPlaceholder = document.getElementById("footerPlaceholder");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "footer.html", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            footerPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+});
