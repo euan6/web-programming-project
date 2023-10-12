526de0bf6cbc84dc6635f27afe1d65a9e85d83a4
Euan <euanconlon46@gmail.com>
2023-10-12 01:56:19 +0100

    Smart watch and Keyboard Image Change

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,13 @@ function changeWatchImage(imageSrc) {
     var image = document.getElementById("watchImage");
     image.src = imageSrc;
 }
+
+function changeKeyboardImage(imageSrc) {
+    var image = document.getElementById("keyboardImage");
+    image.src = imageSrc;
+}
+
+function changeSwatchImage(imageSrc) {
+    var image = document.getElementById("swatchImage");
+    image.src = imageSrc;
+}
