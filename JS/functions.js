608a4b1b05b52a3ee28b0ff2746ad685b08d1291
Euan <euanconlon46@gmail.com>
2023-10-13 01:02:43 +0100

    New JS file created
    Separate files for functions and for xhr

---
+++ JS/functions.js
@@ -0,0 +1,23 @@
+// changes the image of the laptop product
+function changeLaptopImage(imageSrc) {
+    var image = document.getElementById("laptopImage");
+    image.src = imageSrc;
+}
+
+// changes the image of the watch product
+function changeWatchImage(imageSrc) {
+    var image = document.getElementById("watchImage");
+    image.src = imageSrc;
+}
+
+// changes the image of the keyboard product
+function changeKeyboardImage(imageSrc) {
+    var image = document.getElementById("keyboardImage");
+    image.src = imageSrc;
+}
+
+// changes the image of the smart watch product
+function changeSwatchImage(imageSrc) {
+    var image = document.getElementById("swatchImage");
+    image.src = imageSrc;
+}
