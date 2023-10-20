209a4a08a0cdf9d1490bf398f67545c63cb99712
Euan <euanconlon46@gmail.com>
2023-10-20 01:39:04 +0100

    Update products.js
    Descriptions for each product added

--- JS/products.js
+++ JS/products.js
@@ -49,54 +49,54 @@ window.addEventListener("load", function() {
     function() { changeImage('../Images/laptop2.jpg', "laptopImage") }, 
     function() { changeImage('../Images/laptop.jpg', "laptopImage") }, 
     "Laptop", "£799", 
-    "Description for laptop product");
+    "Lenovo Thinkpad E15 with an intel i5 core processor. 32GB RAM and a 256GB SSD.");
 
     // adds watch product
     productDiv("../Images/watch.jpg",  "watchImage", 
     function() { changeImage('../Images/watch2.jpg', "watchImage") },
     function() { changeImage('../Images/watch.jpg', "watchImage") }, 
     "Watch", "£150", 
-    "Description for watch product");
+    "Hugo Boss stainless steel watch with smooth leather wrist strap.");
 
     // adds keyboard product
     productDiv("../Images/keyboard.jpg", "keyboardImage", 
     function() { changeImage('../Images/keyboard2.jpg', "keyboardImage") }, 
     function() { changeImage('../Images/keyboard.jpg', "keyboardImage") }, 
     "Keyboard", "£65", 
-    "Description for keyboard product");
+    "Logitech RGB light up mechanical keyboard with programmable keys.");
 
     // adds smart watch product
     productDiv("../Images/swatch.jpg", "swatchImage", 
     function() { changeImage('../Images/swatch2.jpg', "swatchImage") } , 
     function() { changeImage('../Images/swatch.jpg', "swatchImage") }, 
     "Smart Watch", "£499", 
-    "Description for smart watch product");
+    "New Apple smart watch with touch screen and built in face ID.");
 
     // adds mouse product
     productDiv("../Images/mouse.jpg", "mouseImage", 
     function() { changeImage('../Images/mouse2.jpg', "mouseImage") } , 
     function() { changeImage('../Images/mouse.jpg', "mouseImage") }, 
     "RGB Mouse", "£29", 
-    "Description mouse product");
+    "Ergonomic mouse built to fit the hand with RGB light up edge.");
 
     // adds speaker product
     productDiv("../Images/speaker.jpg", "speakerImage", 
     function() { changeImage('../Images/speaker2.jpg',"speakerImage") } , 
     function() { changeImage('../Images/speaker.jpg', "speakerImage") }, 
     "Logitech Speaker", "£199", 
-    "Description for logitech speaker product");
-
-    // adds headphones product
-    productDiv("../Images/headphones.jpg", "headphonesImage", 
-    function() { changeImage('../Images/headphones2.jpg', "headphonesImage") } , 
-    function() { changeImage('../Images/headphones.jpg', "headphonesImage") }, 
-    "Headphones", "£139", 
-    "Description for headphones product");
+    "100W Logitech speakers with bluetooth capabilities.");
 
     // adds remote product
     productDiv("../Images/remote.jpg", "remoteImage", 
     function() { changeImage('../Images/remote2.jpg', "remoteImage") } , 
     function() { changeImage('../Images/remote.jpg', "remoteImage") }, 
     "TV Remote", "£20", 
-    "Description for tv remote product");
+    "Standard TV remote for any high end smart box.");
+
+    // adds headphones product
+    productDiv("../Images/headphones.jpg", "headphonesImage", 
+    function() { changeImage('../Images/headphones2.jpg', "headphonesImage") } , 
+    function() { changeImage('../Images/headphones.jpg', "headphonesImage") }, 
+    "Headphones", "£139", 
+    "Dr. Dre beats in white with bluetooth capabilities");
 });
