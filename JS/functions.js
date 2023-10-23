052c2495b4cfd1acfaa5bd9faf554eaaa3b73df8
Euan <euanconlon46@gmail.com>
2023-10-24 00:26:09 +0100

    Update functions.js
    Format changed

--- JS/functions.js
+++ JS/functions.js
@@ -39,14 +39,14 @@ function updateClock() {
 
 //
 function updateDay() {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const currentDate = new Date();
     const dayOfWeek = daysOfWeek[currentDate.getDay()];
     const dayOfMonth = currentDate.getDate();
     const month = months[currentDate.getMonth()];
     const year = currentDate.getFullYear();
-    const formattedDate = `Today is ${dayOfWeek} the ${dayOfMonth}${getDaySuffix(dayOfMonth)} of ${month} ${year}`;
+    const formattedDate = `is the current UK time on ${dayOfWeek} the ${dayOfMonth}${getDaySuffix(dayOfMonth)} of ${month} ${year}`;
 
     const currentDayElement = document.getElementById("currentDay");
     if (currentDayElement) {
