80cbedac9aafe401a73bbeaec077eddae013ee4b
Euan <euanconlon46@gmail.com>
2023-10-28 02:11:06 +0100

    Update history.js
    Content for each week updated

--- JS/history.js
+++ JS/history.js
@@ -15,23 +15,23 @@ function historyInfo(description) {
 //adds the p to the 'history class
 window.addEventListener("load", function() {
     // adds p element for week 1
-    historyInfo("Week 1 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 1 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub");
 
     // adds p element for week 2
-    historyInfo("Week 2 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 2 - Skeleton structure created with a header, navbar, main section and a footer");
 
     // adds p element for week 3
-    historyInfo("Week 3 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 3 - New page created for displaying products with product cards and nav bar styling updated");
 
     // adds p element for week 4
-    historyInfo("Week 4 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 4 - Products Page updated along with small styling changes across pages");
 
     // adds p element for week 5
-    historyInfo("Week 5 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 5 - Javascript onmouse functions started for image changes and xhr used to load header, nav bar and footer");
 
     // adds p element for week 6
-    historyInfo("Week 6 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 6 - Forms created for signup and login pages and clock added inside footer");
 
     // adds p element for week 7
-    historyInfo("Week 7 - Basic HTML, CSS and Javascript files were created and uploaded to GitHub.");
+    historyInfo("Week 7 - Media queries added to make more responsive and this page + reviews page created");
 });
