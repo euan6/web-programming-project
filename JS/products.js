4ae1cb5ebc9367db927fa1ec1b9073e784d23593
Euan <euanconlon46@gmail.com>
2023-11-21 02:14:54 +0000

    Image File Paths Changed

--- JS/products.js
+++ JS/products.js
@@ -138,62 +138,62 @@ function productDiv(imageSrc, imageId, imageOver, imageOut, name, price, descrip
 // adds different product divs when the page is loaded
 window.addEventListener("load", function() {
     // adds laptop product
-    productDiv("../Images/laptop.jpg", products[0].id,
-    function() { changeImage('../Images/laptop2.jpg', products[0].id) }, 
-    function() { changeImage('../Images/laptop.jpg', products[0].id) }, 
+    productDiv("Images/laptop.jpg", products[0].id,
+    function() { changeImage('Images/laptop2.jpg', products[0].id) }, 
+    function() { changeImage('Images/laptop.jpg', products[0].id) }, 
     products[0].name, products[0].price, products[0].description, products[0].num);
 
     // adds watch product
-    productDiv("../Images/watch.jpg",  products[1].id, 
-    function() { changeImage('../Images/watch2.jpg', products[1].id) },
-    function() { changeImage('../Images/watch.jpg', products[1].id) }, 
+    productDiv("Images/watch.jpg",  products[1].id, 
+    function() { changeImage('Images/watch2.jpg', products[1].id) },
+    function() { changeImage('Images/watch.jpg', products[1].id) }, 
     products[1].name, products[1].price, products[1].description, products[1].num);
 
     // adds keyboard product
-    productDiv("../Images/keyboard.jpg", products[2].id, 
-    function() { changeImage('../Images/keyboard2.jpg', products[2].id) }, 
-    function() { changeImage('../Images/keyboard.jpg', products[2].id) }, 
+    productDiv("Images/keyboard.jpg", products[2].id, 
+    function() { changeImage('Images/keyboard2.jpg', products[2].id) }, 
+    function() { changeImage('Images/keyboard.jpg', products[2].id) }, 
     products[2].name, products[2].price, products[2].description, products[2].num);
 
     // adds smart watch product
-    productDiv("../Images/swatch.jpg", products[3].id, 
-    function() { changeImage('../Images/swatch2.jpg', products[3].id) } , 
-    function() { changeImage('../Images/swatch.jpg', products[3].id) }, 
+    productDiv("Images/swatch.jpg", products[3].id, 
+    function() { changeImage('Images/swatch2.jpg', products[3].id) } , 
+    function() { changeImage('Images/swatch.jpg', products[3].id) }, 
     products[3].name, products[3].price, products[3].description, products[3].num);
 
     // adds mouse product
-    productDiv("../Images/mouse.jpg", products[4].id, 
-    function() { changeImage('../Images/mouse2.jpg', products[4].id) } , 
-    function() { changeImage('../Images/mouse.jpg', products[4].id) }, 
+    productDiv("Images/mouse.jpg", products[4].id, 
+    function() { changeImage('Images/mouse2.jpg', products[4].id) } , 
+    function() { changeImage('Images/mouse.jpg', products[4].id) }, 
     products[4].name, products[4].price, products[4].description, products[4].num);
 
     // adds speaker product
-    productDiv("../Images/speaker.jpg", products[5].id, 
-    function() { changeImage('../Images/speaker2.jpg', products[5].id) } , 
-    function() { changeImage('../Images/speaker.jpg', products[5].id) }, 
+    productDiv("Images/speaker.jpg", products[5].id, 
+    function() { changeImage('Images/speaker2.jpg', products[5].id) } , 
+    function() { changeImage('Images/speaker.jpg', products[5].id) }, 
     products[5].name, products[5].price, products[5].description, products[5].num);
 
     // adds remote product
-    productDiv("../Images/remote.jpg", products[6].id, 
-    function() { changeImage('../Images/remote2.jpg', products[6].id) } , 
-    function() { changeImage('../Images/remote.jpg', products[6].id) }, 
+    productDiv("Images/remote.jpg", products[6].id, 
+    function() { changeImage('Images/remote2.jpg', products[6].id) } , 
+    function() { changeImage('Images/remote.jpg', products[6].id) }, 
     products[6].name, products[6].price, products[6].description, products[6].num);
 
     // adds headphones product
-    productDiv("../Images/headphones.jpg", products[7].id, 
-    function() { changeImage('../Images/headphones2.jpg', products[7].id) } , 
-    function() { changeImage('../Images/headphones.jpg', products[7].id) }, 
+    productDiv("Images/headphones.jpg", products[7].id, 
+    function() { changeImage('Images/headphones2.jpg', products[7].id) } , 
+    function() { changeImage('Images/headphones.jpg', products[7].id) }, 
     products[7].name, products[7].price, products[7].description, products[7].num);
 
     // adds phone holder product
-    productDiv("../Images/holder.jpg", products[8].id, 
-    function() { changeImage('../Images/holder2.jpg', products[8].id) } , 
-    function() { changeImage('../Images/holder.jpg', products[8].id) }, 
+    productDiv("Images/holder.jpg", products[8].id, 
+    function() { changeImage('Images/holder2.jpg', products[8].id) } , 
+    function() { changeImage('Images/holder.jpg', products[8].id) }, 
     products[8].name, products[8].price, products[8].description, products[8].num);
 
     // adds monitor product
-    productDiv("../Images/monitor.jpg", products[9].id, 
-    function() { changeImage('../Images/monitor2.jpg', products[9].id) } , 
-    function() { changeImage('../Images/monitor.jpg', products[9].id) }, 
+    productDiv("Images/monitor.jpg", products[9].id, 
+    function() { changeImage('Images/monitor2.jpg', products[9].id) } , 
+    function() { changeImage('Images/monitor.jpg', products[9].id) }, 
     products[9].name, products[9].price, products[9].description, products[9].num);
 });
