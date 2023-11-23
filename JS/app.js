479169d86e9d676cf5661e2c287ba8cb6d11289c
Euan <euanconlon46@gmail.com>
2023-11-23 10:07:51 +0000

    Create app.js
    Express code added

---
+++ JS/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = null; 
+
+const db = require('./db');
+
+app.get('/', (req, res) => {
+  db.query('SELECT * FROM your_table', (err, results) => {
+    if (err) {
+      console.error('Error querying the database: ' + err.stack);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.send(`<pre>${JSON.stringify(results, null, 2)}</pre>`);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
